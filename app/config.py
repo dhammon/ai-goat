349c0fd25b4bc3557ee15a3df73847d7210b2bdb
Guanwei Hu <guanwei.woo@gmail.com>
2024-08-21 13:04:50 +0800

    add model file md5sum check

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@
 class Config:
     MODEL_PATH = "app/models/ggml-old-vic13b-q4_0.bin"
     MODEL_URL = "https://huggingface.co/vicuna/ggml-vicuna-13b-1.1/resolve/main/ggml-old-vic13b-q4_0.bin"
+    MODEL_MD5 = "dfe5d1e30242d63a72d06f7bee5edf80"
     AI_IMAGES = [
         "rootcauz/ai-base:0.0.1",
         "rootcauz/ai-ctfd:0.0.2"
